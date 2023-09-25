daa31a78b6e61c8d2d7ed0cba0640138a47c3cbe
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-09-25 18:20:53 -0300

    Finalizando aula sobre rede e Api

--- app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
@@ -1,20 +1,49 @@
 package com.example.eletriccarapp.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletriccarapp.R
-import com.example.eletriccarapp.data.carFactory
+import com.example.eletriccarapp.data.CarsApi
+import com.example.eletriccarapp.domain.Carro
 import com.example.eletriccarapp.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONArray
+import org.json.JSONTokener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.Objects
 
 class CarFragment : Fragment() {
     lateinit var listaCarros: RecyclerView
     lateinit var fabCalcular: FloatingActionButton
+    lateinit var progress: ProgressBar
+    lateinit var noInternetImage : ImageView
+    lateinit var noInternetText : TextView
+    lateinit var carsApi: CarsApi
+
+    var carrosArray: ArrayList<Carro> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,29 +55,183 @@ class CarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRetrofit()
         getCampos(view)
-        setLista()
         setListener()
     }
 
-   fun getCampos(view: View) {
+    override fun onResume() {
+        super.onResume()
+        if(checkForInternet(context)) {
+            emptyState(false)
+            //callService() -> outra forma de chamar serviço
+            getAllCars()
+        } else {
+            emptyState(true)
+        }
+    }
+
+    fun setupRetrofit() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://igorbag.github.io/cars-api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        carsApi = retrofit.create(CarsApi::class.java)
+    }
+
+    fun getAllCars () {
+        carsApi.getAllCars().enqueue(object: Callback<List<Carro>> {
+            override fun onResponse(call: Call<List<Carro>>, response: Response<List<Carro>>) {
+                if (response.isSuccessful) {
+                    progress.isVisible = true
+                    response.body()?.let {
+                        progress.isVisible = false
+                        setLista(it)
+                    }
+                } else {
+                    Toast.makeText(context,R.string.response_error,Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Carro>>, t: Throwable) {
+                Toast.makeText(context,R.string.response_error,Toast.LENGTH_LONG).show()
+            }
+
+        } )
+    }
+
+    private fun emptyState(visibility: Boolean) {
+        progress.visibility = View.GONE
+        listaCarros.visibility = View.GONE
+        noInternetImage.isVisible = visibility
+        noInternetText.isVisible = visibility
+
+    }
+
+
+    fun getCampos(view: View) {
        view?.apply {
            listaCarros = findViewById(R.id.rv_lista_carros)
            fabCalcular = findViewById(R.id.fab_calcular)
+           progress = findViewById(R.id.pb_loading)
+           noInternetText = findViewById(R.id.tv_no_internet)
+           noInternetImage = findViewById(R.id.iv_empty_state)
        }
-
    }
 
-   fun setLista () {
-       val dados = carFactory().list
-       val adapter = CarAdapter(dados)
-       listaCarros.adapter = adapter
+   fun setLista (lista: List<Carro>) {
+       val newAdapter = CarAdapter(lista)
+       listaCarros.apply {
+           adapter = newAdapter
+           visibility = View.VISIBLE
+       }
    }
 
     fun setListener() {
         fabCalcular.setOnClickListener {
             startActivity(Intent(context, CalcularAutonomiaApp::class.java))
         }
+    }
+
+    fun callService () {
+        GetCarInformation().execute("https://igorbag.github.io/cars-api/cars.json")
+    }
+
+    fun checkForInternet (context: Context?) : Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork?: return false
+
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network)?: return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return  networkInfo.isConnected
+        }
+    }
+
+    // Utilizando o retrofit como abstração da async task
+    inner class GetCarInformation : AsyncTask<String, String, String>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progress.visibility = View.VISIBLE
+        }
+        override fun doInBackground(vararg url: String?): String {
+            var urlConnection: HttpURLConnection? = null
+
+            try {
+                val urlBase = URL(url[0])
+                urlConnection = urlBase.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 60000
+                urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty("Accept", "application/json")
+
+                val responseCode = urlConnection.responseCode
+
+                if(responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                } else {
+                    Log.e("Erro", "serviço indisponivel no momento")
+                }
+
+
+            } catch (ex: Exception) {
+                Log.e("Erro", "erro ao realizar processamento")
+            } finally {
+                urlConnection?.disconnect()
+            }
+
+            return ""
+        }
+
+        override fun onProgressUpdate(vararg values: String?) {
+
+            try {
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+
+                for (i in 0 until jsonArray.length()){
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    val preco = jsonArray.getJSONObject(i).getString("preco")
+                    val bateria = jsonArray.getJSONObject(i).getString("bateria")
+                    val potencia = jsonArray.getJSONObject(i).getString("potencia")
+                    val recarga = jsonArray.getJSONObject(i).getString("recarga")
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+
+                    val model = Carro(
+                        id = id.toInt(),
+                        preco= preco,
+                        bateria = bateria,
+                        potencia = potencia,
+                        recarga = recarga,
+                        urlPhoto = urlPhoto
+                    )
+
+                    carrosArray.add(model)
+
+                }
+                progress.visibility = View.INVISIBLE
+                //setLista()
+
+
+
+            } catch (err: Exception) {
+                Log.e("erro",err.message.toString())
+            }
+        }
+
+
+
 
     }
 }
